54b890984732d47082b1c6c801adb15598d5f510
trendpeek <86482071+trendpeek@users.noreply.github.com>
2022-07-24 12:51:43 +0500

    Update user_account.py

--- app/user_account.py
+++ app/user_account.py
@@ -34,34 +34,39 @@ class user_account:
         return balance
 
     def isProvidingLiqBALN(self, address) -> bool:
-        isLP = network.isEarningRewards(address, 3) or network.isEarningRewards(address, 4)
+        isLP = 0 #network.isEarningRewards(address, 3) or network.isEarningRewards(address, 4)
         return bool(isLP)
 
     def updateUser(self, address, day):
         self.address = address
         self.balance = self.balanceData(address)
-        self.isLP = self.isProvidingLiqBALN(address)
+        self.isLP = 0 #self.isProvidingLiqBALN(address)
         self.iseligible = self.isLP or (self.balance["staked"] > 0)
 
-        last_dividents = network.getUserDividends(address, day - 1, day)
+        # last_dividents = network.getUserDividends(address, day - 1, day)
         total_dividents = tuple({})
-        for x in range(1, day, 50):
-            if day < x + 50:
-                total_dividents = total_dividents + network.getUserDividends(address, x, day)
-            else:
-                total_dividents = total_dividents + network.getUserDividends(address, x, x + 49)
-        self.lastBALNdividents = int(last_dividents[0][network.BALN_CONTRACT], 16) / EXA
-        self.lastBNUSDdividents = int(last_dividents[0][network.BNUSD_CONTRACT], 16) / EXA
-        self.lastSICXdividents = int(last_dividents[0][network.SICX_CONTRACT], 16) / EXA
-        for x in range(0, len(total_dividents), 2):
-            self.totalBALNdividents = self.totalBALNdividents + \
-                                      (int(total_dividents[x][network.BALN_CONTRACT], 16)/EXA)
-            self.totalBNUSDdividents = self.totalBNUSDdividents + \
-                                       (int(total_dividents[x][network.BNUSD_CONTRACT], 16)/EXA)
-            self.totalSICXdividents = self.totalSICXdividents + \
-                                      (int(total_dividents[x][network.SICX_CONTRACT], 16) /EXA)
-
+        total_dividents = network.getUnclaimedDividends(address)
+        
+        # for x in range(1, day, 50):
+        #     if day < x + 50:
+        #         total_dividents = total_dividents + network.getUserDividends(address, x, day)
+        #     else:
+        #         total_dividents = total_dividents + network.getUserDividends(address, x, x + 49)
+        # self.lastBALNdividents = int(last_dividents[0][network.BALN_CONTRACT], 16) / EXA
+        # self.lastBNUSDdividents = int(last_dividents[0][network.BNUSD_CONTRACT], 16) / EXA
+        # self.lastSICXdividents = int(last_dividents[0][network.SICX_CONTRACT], 16) / EXA
+        # for x in range(0, len(total_dividents), 2):
+            # self.totalBALNdividents = self.totalBALNdividents + \
+            #                             (int(total_dividents[x][network.BALN_CONTRACT], 16)/EXA)
+            # self.totalBNUSDdividents = self.totalBNUSDdividents + \
+            #                             (int(total_dividents[x][network.BNUSD_CONTRACT], 16)/EXA)
+            # self.totalSICXdividents = self.totalSICXdividents + \
+            #                             (int(total_dividents[x][network.SICX_CONTRACT], 16) /EXA)
+        self.totalBALNdividents = int(total_dividents[0][network.BALN_CONTRACT], 16)/EXA
+        self.totalBNUSDdividents = int(total_dividents[0][network.BNUSD_CONTRACT], 16)/EXA
+        self.totalSICXdividents = int(total_dividents[0][network.SICX_CONTRACT], 16) /EXA
         return True
+
 
     def updateUserClaim(self, claimData, networkInfo):
         for i in range(len(claimData)-1, -1, -1):
